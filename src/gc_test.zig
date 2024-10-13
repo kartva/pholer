e66cb8e740f60547dbbd444926bfec7a3e904414
Mukul Agarwal <agarmukul23@gmail.com>
2024-10-12 21:20:00 -0400

    major restructure

--- src/gc_test.zig
+++ src/gc_test.zig
@@ -4,31 +4,30 @@ const main = @import("main.zig");
 const allocator = std.testing.allocator;
 
 test "garbage collector should deallocate unreachable nodes" {
+    // Initialize memory manager
+    const mgr = try main.MemoryManager.init();
     // Initialize root node
-    var root = try main.Node.init();
-    try main.GlobalRootList.append(root);
+    var root = try main.Node.init(mgr);
 
     // Initialize child node and add to root
-    const child = try main.Node.init();
+    const child = try main.Node.init(mgr);
     try root.add_child(child);
 
     // Initialize another node that is not connected to the root
-    const orphan = try main.Node.init();
+    const orphan = try main.Node.init(mgr);
 
     // Perform garbage collection
-    try main.garbage_collect();
+    try mgr.garbage_collect();
 
     // Check that the orphan node has been deallocated
     var orphan_found = false;
-    for (main.GlobalNodeList.items) |node| {
-        if (node == orphan) {
+    var it = mgr.nodeSet.keyIterator();
+    while (it.next()) |node| {
+        if (node.* == orphan) {
             orphan_found = true;
             break;
         }
     }
-    try std.testing.expect(!orphan_found);
 
-    // Clean up
-    main.GlobalRootList.deinit();
-    main.GlobalNodeList.deinit();
+    try std.testing.expect(!orphan_found);
 }
--- src/main.zig
+++ src/main.zig
@@ -2,54 +2,83 @@ const std = @import("std");
 
 const allocator = std.heap.page_allocator;
 
-// all nodes created
-pub var GlobalNodeList = std.ArrayList(*Node).init(allocator);
+pub const MemoryManager: type = struct {
+    clearList: std.ArrayList(*Node),
+    nodeSet: std.AutoHashMap(*Node, void),
+    rootSet: std.AutoHashMap(*Node, void),
 
-// all reachable nodes
-pub var GlobalRootList = std.ArrayList(*Node).init(allocator);
+    pub fn init() !*MemoryManager {
+        const m: *MemoryManager = try allocator.create(MemoryManager);
+        m.clearList = std.ArrayList(*Node).init(allocator);
+        m.nodeSet = std.AutoHashMap(*Node, void).init(allocator);
+        m.rootSet = std.AutoHashMap(*Node, void).init(allocator);
+        return m;
+    }
+
+    pub fn addNode(self: *MemoryManager, node: *Node) !void {
+        try self.rootSet.put(node, {});
+        node.visited = false;
+    }
+
+    pub fn addRoot(self: *MemoryManager, node: *Node) !void {
+        self.nodeSet.put(node);
+        self.rootSet.put(node);
+        node.visited = false;
+    }
+
+    pub fn garbage_collect(self: *MemoryManager) !void {
+        // mark all nodes
+        var it = self.rootSet.keyIterator();
+        while (it.next()) |node| {
+            try node.*.mark();
+        }
+        // sweep all nodes
+        var it2 = self.nodeSet.keyIterator();
+        while (it2.next()) |node| {
+            if (!node.*.visited) {
+                try self.clearList.append(node.*);
+            }
+            node.*.visited = false;
+        }
+
+        for (self.clearList.items) |node| {
+            _ = self.nodeSet.remove(node);
+            try node.cleanup();
+        }
+
+        // clear out the clearing list
+        self.clearList.shrinkRetainingCapacity(0);
+    }
+};
 
 pub const Node = struct {
     children: std.ArrayList(*Node),
     data: [512]u8 = undefined, // empty array initialization to 0
     visited: bool = false,
 
-    pub fn init() !*Node {
+    pub fn init(mem: *MemoryManager) !*Node {
         const n: *Node = try allocator.create(Node);
-        try GlobalNodeList.append(n); // always keeping track of all nodes
         n.children = std.ArrayList(*Node).init(allocator);
+        try mem.addNode(n);
         return n;
     }
 
     pub fn add_child(self: *Node, child: *Node) !void {
         try self.children.append(child);
     }
-};
 
-fn mark(node: *Node) !void {
-    if (node.visited) {
-        return;
-    }
-    node.visited = true;
-    for (node.children.items) |child| {
-        try mark(child);
-    }
-}
-
-pub fn garbage_collect() !void {
-    for (GlobalNodeList.items) |node| {
-        node.visited = false;
-    }
-
-    for (GlobalRootList.items) |node| {
-        try mark(node);
-    }
-
-    for (GlobalNodeList.items, 0..GlobalNodeList.items.len) |node, idx| {
-        if (!node.visited) {
-            allocator.destroy(node);
-            _ = GlobalNodeList.orderedRemove(idx);
+    fn mark(node: *Node) !void {
+        if (node.visited) {
+            return;
+        }
+        node.visited = true;
+        for (node.children.items) |child| {
+            try mark(child);
         }
     }
-}
 
+    fn cleanup(node: *Node) !void {
+        node.visited = false;
+    }
+};
 pub fn main() !void {}
